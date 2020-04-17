e0b806212a61c1f1c7e0da8c63c619336f214320
victory_free <dinggl9367@163.com>
2020-04-17 21:02:59 +0800

    update webserver, using fork() function

--- webserver.c
+++ webserver.c
@@ -8,22 +8,36 @@
 #include "socklib.h"
 #include <time.h>
 #include <string.h>
+#include <signal.h>
+#include <errno.h>
+
+#define PORTNUM 8888
+
 
 /*do handle the request sending by client */
 void process_request(int fd);
-
+void child_waiter(int signum); /*receive SIGCHILD signal handler*/
 int main() {
 
     int webserver_socket, fd_communicate;
-    webserver_socket = make_server_socket(8888);
+
+    /*set what handler will call, when receive SIGCHILD signal*/
+    signal(SIGCHLD, child_waiter);
+
+    webserver_socket = make_server_socket(PORTNUM);
     if(webserver_socket < 0)
         return -1;
 
     while(1)
     {
         fd_communicate = accept(webserver_socket, NULL, NULL);
-        if(fd_communicate < 0)
-            break;
+        if(fd_communicate < 0 )
+        {
+            if (errno == EINTR)         //accept interupted
+                break;
+            else
+                continue;
+        }
 
         /*debug*/
         printf("accept successs: fd:%d\n", fd_communicate);
@@ -40,10 +54,28 @@ int main() {
 
 void process_request(int fd)
 {
-    time_t now;
-    char* tm_str = NULL;
+    pid_t pid;
 
-    time(&now);
-    tm_str = ctime(&now);
-    write(fd, tm_str, strlen(tm_str));
+    pid = fork();
+    switch (pid)
+    {
+        case -1:
+            return;
+        case 0:
+            dup2(fd, 1);
+            close(fd);
+            execl("/bin/date", "date", NULL);
+            //oops("execlp");
+        default:
+            wait(NULL);
+    }
+
 }
+
+void child_waiter(int signum)
+{
+    //wait all child process exit, then clear child process source
+    while(waitpid(-1, NULL, WNOHANG) > 0);
+}
+
+
