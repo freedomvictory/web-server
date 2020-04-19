7b18788362a8e1cdc0dfca4ac172361cbaa82d45
victory_free <dinggl9367@163.com>
2020-04-19 20:19:48 +0800

    update webserver.c, implement do_404() and do_ls() function

--- webserver.c
+++ webserver.c
@@ -10,23 +10,25 @@
 #include <string.h>
 #include <signal.h>
 #include <errno.h>
+#include <sys/stat.h>
+#include <stdlib.h>
 
 #define PORTNUM 8888
 
 
 /*do handle the request sending by client */
 void process_request(char* rq, int fd);
-void child_waiter(int signum); /*receive SIGCHILD signal handler*/
+void child_waiter(int signum);              /*receive SIGCHILD signal handler*/
 void read_til_crnl(FILE *fp);
 void cannot_do(int fd);
-int not_exist(char* arg);
-void do_404(char *arg, int fd);
-int is_dir(char *arg);
-void do_ls(char *arg, int fd);
+int not_exist(char* arg);                   /*judge Is the file exist in web server?*/
+void do_404(char *item, int fd);             /*web server do 404 response to client*/
+int is_dir(char *file);                      /*judge Is file a dir? */
+void do_ls(char *arg, int fd);              /*do ls command*/
 int ends_in_cgi(cha *arg);
-void do_exec(char *arg, int fd);
-void do_cat(char *arg, int fd);
-
+void do_exec(char *arg, int fd);            /*do exec command*/
+void do_cat(char *arg, int fd);             /*do cat command*/
+void header(FILE *fp, char *content_type); /*the reply header*/
 int main() {
 
     int webserver_socket, fd_communicate;
@@ -68,6 +70,11 @@ int main() {
     return 0;
 }
 
+/*
+ * handle the request from client
+ * rq: request string
+ * fd: communicate socket file descriptor
+ * */
 void process_request(char* rq, int fd)
 {
     char	cmd[BUFSIZ], arg[BUFSIZ];
@@ -92,36 +99,127 @@ void process_request(char* rq, int fd)
         do_cat( arg, fd );
 
 }
-
+/*
+ * singnal handler when recevie SIGCHILD signal
+ * signum:
+ * */
 void child_waiter(int signum)
 {
-    //wait all child process exit, then clear child process source
+    //wait all child process exit, then clear child process memory source
     while(waitpid(-1, NULL, WNOHANG) > 0);
 }
-
+/* ------------------------------------------------------ *
+   read_til_crnl(FILE *)
+   skip over all request info until a CRNL is seen
+   ------------------------------------------------------ */
 void read_til_crnl(FILE *fp)
 {
-
+    char buf[BUFSIZ];
+    while(fgets(buf, BUFSIZ, fp) != NULL && strcmp(buf, "\r\n") != 0);
 }
+/* ------------------------------------------------------ *
+   the reply header thing: all functions need one
+   if content_type is NULL then don't send content type
+   ------------------------------------------------------ */
+
+void header( FILE *fp, char *content_type )
+{
+    fprintf(fp, "HTTP/1.0 200 OK\r\n");
+    if ( content_type )
+    fprintf(fp, "Content-type: %s\r\n", content_type );
+}
+
+/* what did server do when receive request which not supported
+ * fd: communicate file descriptor
+ * */
+
 void cannot_do(int fd)
 {
+    FILE *fp;
+    fp = fdopen(fd, "w");
+    if(fp == NULL)
+    {
+        //error occur: can't open fd as file stream.
+        return;
+    }
+    fprintf(fp, "HTTP/1.0 501 Not Implemented\r\n");
+    fprintf(fp, "Content-type: text/plain\r\n");
+    fprintf(fp, "\r\n");
+
+    fprintf(fp, "That command is not yet implemented\r\n");
+    fclose(fp);
 
 }
+
+/******************************************
+ *  judge whether the file exist?
+ * @param arg : file name
+ * @return 1: not exist ; 0: exist
+ */
+
 int not_exist(char* arg)
 {
-    return 0;
+    struct stat info;
+    return (stat(arg, &info) == -1);
 }
-void do_404(char *arg, int fd)
+
+/****************************************
+ * web server do 404 response to client browser
+ * @param item :file name
+ * @param fd  :communicate socket file disc
+ */
+void do_404(char *item, int fd)
 {
+    FILE	*fp = fdopen(fd,"w");
+    if(fp == NULL)
+    {
+        //error occurs: can't open file
+        return;
+    }
+
+    fprintf(fp, "HTTP/1.0 404 Not Found\r\n");
+    fprintf(fp, "Content-type: text/plain\r\n");
+    fprintf(fp, "\r\n");
+
+    fprintf(fp, "The item you requested: %s\r\nis not found\r\n",
+            item);
+    fclose(fp);
 
 }
-int is_dir(char *arg)
+/*************************************
+ * judge whether is it a directory ?
+ * @param arg : dir path name
+ * @return : 0 is dir, 1 is others
+ */
+int is_dir(char *file)
 {
-    return 0;
+    struct stat info;
+    return ( stat(f, &info) != -1 && S_ISDIR(info.st_mode) );
 }
+/**************************************
+ * web server do ls command
+ * @param arg :dir path name
+ * @param fd  :communicate socket
+ */
+
 void do_ls(char *arg, int fd)
 {
+    FILE* fp;
+    fp = fdopen(fd, "w");
+    if(fp == NULL)
+    {
+        //error occurs
+        return;
+    }
+    header(fp, "text/plain");
+    fprintf(fp,"\r\n");
+    fflush(fp);
 
+    dup2(fd, 1);
+    dup2(fd, 2);
+    execlp("ls", "ls", "-l", arg, NULL);
+    perror(arg);
+    exit(1);
 }
 int ends_in_cgi(cha *arg)
 {
